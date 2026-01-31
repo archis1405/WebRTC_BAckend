7605b09207c601e3fce3982d8058260aa2b1128e
archis1405 <archismandas1405@gmail.com>
2026-01-31 19:13:26 +0530

    Added the video feed

--- src/handlers/roomHandler.ts
+++ src/handlers/roomHandler.ts
@@ -2,11 +2,10 @@ import { Socket } from "socket.io";
 import { v4 as uuidv4 } from "uuid";
 import IRoomParams from "../interfaces/iRoomParams";
 
+// this will hold the roomId as key and array of socket ids as value i.e room and the users in that room
+const rooms: Record<string, string[]> = {};
+
 const roomHandler = (socket: Socket) => {
-
-    // this will hold the roomId as key and array of socket ids as value i.e room and the users in that room 
-    const rooms : Record<string, string[]> = {};
-
 
     const createRoom = () => {
         // this will be the unique room id which can be shared with others to join
@@ -14,14 +13,14 @@ const roomHandler = (socket: Socket) => {
         // we will make socket connections to join the room
         socket.join(roomId);
 
-        rooms[roomId]= [];
+        rooms[roomId] = [];
         // we will emit an event from the server side that a socket connection has been added to the room
         socket.emit("room-created", { roomId });
         console.log(`Room created with ID: ${roomId}`);
     };
 
     const joinedRoom = ({ roomId, peerId }: IRoomParams) => {
-        if(rooms[roomId]) {
+        if (rooms[roomId]) {
             console.log(`New user has joined the room: ${roomId} with Peer ID: ${peerId}`);
             rooms[roomId].push(peerId);
 
@@ -30,9 +29,9 @@ const roomHandler = (socket: Socket) => {
             socket.emit("get-users" , {
                 roomId,
                 participants: rooms[roomId]
-            })
+            });
         }
-        
+
     };
 
     // we will call the createRoom function when the event is emitted from the client side
